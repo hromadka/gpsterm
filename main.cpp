aa79ca4390ddf04920e4629c0e77cf6714d08be6
Ted Hromadka <hromadka@gmail.com>
2022-02-09 08:26:38 -0800

    start adding input args

--- main.cpp
+++ main.cpp
@@ -6,8 +6,25 @@ using namespace mn::CppLinuxSerial;
 
 int main(int argc, char ** argv){
 	
-	for(int i = 0; i < argc; i++)
-		printf("Argument %d: %s\n", (i+1), argv[i]);
+	for(int i = 1; i < argc; i++) {
+		// print all input params to confirm
+		//printf("Argument %d: %s\n", (i+1), argv[i]);
+		std::string arg = argv[i];
+        if ((arg == "-h") || (arg == "--help")) {
+            std::cout << "GPSTERM is a simple utility to read incoming NMEA strings from an attached GPS over a _
+			serial port that was specified using the '-p <PORT>' flag.  Without this flag, the default port is /dev/ttyUSB0." << std::endl;
+            return 0;
+        } else if ((arg == "-d") || (arg == "--destination")) {
+            if (i + 1 < argc) { // Make sure we aren't at the end of argv!
+                destination = argv[i++]; // Increment 'i' so we don't get the argument as the next argv[i].
+            } else { // Uh-oh, there was no argument to the destination option.
+                  std::cerr << "--destination option requires one argument." << std::endl;
+                return 1;
+            }  
+        } else {
+            sources.push_back(argv[i]);
+        }		
+	}
 		
 	// Create serial port object 
 	SerialPort serialPort("/dev/ttyUSB1", BaudRate::B_4800, NumDataBits::EIGHT, Parity::NONE, NumStopBits::ONE);
@@ -28,9 +45,11 @@ int main(int argc, char ** argv){
 		serialPort.Read(readData);
 		strcpy(char_array, readData.c_str());
 
+		// look for endline trigger and take action when found
  		length = sizeof(readData);
 		for (int i = 0; i < length; i++) {
         	if (char_array[i] == '\n') {
+				// do action(s) here
         		std::cout << std::endl;
         	}
         }

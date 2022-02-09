30177ad239ce86bacc647ba6f73016f88373414b
Ted Hromadka <hromadka@gmail.com>
2022-02-09 08:08:25 -0800

    clean up code a little

--- main.cpp
+++ main.cpp
@@ -5,24 +5,21 @@
 using namespace mn::CppLinuxSerial;
 
 int main() {
-	// Create serial port object and open serial port at 57600 buad, 8 data bits, no parity bit, and one stop bit (8n1)
+	// Create serial port object 
 	SerialPort serialPort("/dev/ttyUSB0", BaudRate::B_4800, NumDataBits::EIGHT, Parity::NONE, NumStopBits::ONE);
-	// Use SerialPort serialPort("/dev/ttyACM0", 13000); instead if you want to provide a custom baud rate
 	serialPort.SetTimeout(-1); // Block when reading until any data is received
 	
 	std::cout << "opening serial port" << std::endl;
 	
 	serialPort.Open();
 
-	// Write some ASCII data
-	//serialPort.Write("Hello");
-
-	// Read some data back (will block until at least 1 byte is received due to the SetTimeout(-1) call above)
 	
 	std::cout << "waiting for first endline character" << std::endl;
 	
 	int length = 0;
 	char char_array[1028];
+	// Read some data back (will block until at least 1 byte is received due to the SetTimeout(-1) call above).
+	// TODO: add timer to allow graceful exit from while(1)
 	while (1) {
 		std::string readData;
 		serialPort.Read(readData);

c6a622a00a105113082b95638e4a064a7af4994a
Ted Hromadka <hromadka@gmail.com>
2022-02-09 12:59:46 -0800

    add more helper info

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ using namespace mn::CppLinuxSerial;
 int main(int argc, char ** argv){
 	std::string gpsport = "ttyUSB0";
 	bool simulation_mode = false;
+	int provider_id = 0;
+	float version_num = 0.1;
 
 	for(int i = 1; i < argc; i++) {
 		// print all input params to confirm
@@ -15,11 +17,25 @@ int main(int argc, char ** argv){
 	
 		std::string arg = argv[i];
         if ((arg == "-h") || (arg == "--help")) {
-            std::cout << "'gpsterm' is a simple utility to read incoming NMEA strings from an attached GPS over a serial port." << std::endl;
-			std::cout << "Input arguments:"  << std::endl;
-			std::cout << "  -p <USB_port> specifies USB port under /dev, e.g. '-p ttyUSB1'.  Without this flag, the default port is /dev/ttyUSB0." << std::endl;
-			std::cout << "  -s controls simulation mode.  If '-s' flag is used, gpsterm reports a simulated position with random walk.  " << std::endl;
+            std::cout << "'gpsterm' is a simple utility to read incoming NMEA strings from an attached serial GPS device." << std::endl << std::endl;
+			std::cout << "Usage:" << std::endl;
+			std::cout << "  ./gpsterm [options]" << std::endl << std::endl;
+			std::cout << "Input arguments:" << std::endl;
+			std::cout << "  -h              help" << std::endl;
+			std::cout << "  -i              sets ID.  Default ID is 0." << std::endl;
+			std::cout << "  -p <USB_port>   specifies USB port under /dev, e.g. '-p ttyUSB1'.  Without this flag, the default port is /dev/ttyUSB0." << std::endl;
+			std::cout << "  -s              controls simulation mode.  If '-s' flag is used, gpsterm reports a simulated position with random walk.  " << std::endl;
+			std::cout << "  -v 				version info." << std::endl;
+			std::cout << std::endl;
             return 0;
+		} else if ((arg == "-i") || (arg == "--id")) {
+            if (i + 1 < argc) { 
+                provider_id = argv[i+1];
+				i++; 
+            } else { 
+                std::cerr << "--id option requires a value!" << std::endl;
+                return 1;
+            }  
         } else if ((arg == "-p") || (arg == "--port")) {
             if (i + 1 < argc) { 
                 gpsport = argv[i+1];
@@ -30,14 +46,22 @@ int main(int argc, char ** argv){
             }  
 		} else if ((arg == "-s") || (arg == "--sim")) {
 			simulation_mode = true; // technically, only need "-s", without another arg, unless want to pass starting coords this way.
+		} else if ((arg == "-v") || (arg == "--version")) {
+			std::cout << version_num << std::endl;
+			return 0;
         } else {
-            std::cout << "unrecognized input parameter: " << argv[i] << std::endl;
+            std::cerr << "unrecognized input parameter: " << argv[i] << std::endl;
+			return 1;
         }
 				
 	}
-		
-	std::cout << "using port /dev/" + gpsport << std::endl;	
-	std::cout << "simulation mode = " << simulation_mode << std::endl;
+
+	std::cout << "id = " << provider_id << std::endl;
+	if (simulation_mode) {
+		std::cout << "gpsport: simulation mode = " << simulation_mode << std::endl;
+	} else {
+		std::cout << "gpsport: using port /dev/" + gpsport << std::endl;
+ 	}
 
 
 	if (simulation_mode) {
